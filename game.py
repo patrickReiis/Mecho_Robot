98117adae84cef26fe81cf393bf4e00d7edf2133
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-02-08 17:04:25 -0300

    Close window setup

--- game.py
+++ game.py
@@ -2,11 +2,28 @@ import pygame, sys
 from pygame.locals import *
 
 
-DISPLAYSURF = pygame.display.set_mode((700,600))
+DISPLAYSURF = pygame.display.set_mode((1024,760))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+def exit():
+    pygame.quit()
+    sys.exit()
+
+def checkQuit():
+    for event in pygame.event.get(QUIT):
+        exit()
+
+    for event in pygame.event.get(KEYDOWN):
+        if event.key == K_ESCAPE:
+            exit()
+        if event.mod == KMOD_LCTRL and event.key == K_w:
+            exit()
+        pygame.event.post(event) # put the other KEYDOWN events back to the list
+
+def main():
+    while True:
+        checkQuit()
+
+if __name__ == main():
+    main()
+        
 
