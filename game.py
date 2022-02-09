d0a56cc2c1e4eaed6ebd3efaf6fc6e668e4d115b
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-02-09 16:38:42 -0300

    Screen resizable feature

--- game.py
+++ game.py
@@ -1,27 +1,29 @@
 import pygame, sys
 from pygame.locals import *
 
+flags = pygame.SCALED|pygame.RESIZABLE
+DISPLAYSURF = pygame.display.set_mode((1280,720),flags)
 
-DISPLAYSURF = pygame.display.set_mode((1024,760))
-
-def exit():
+def terminate():
     pygame.quit()
     sys.exit()
 
 def checkQuit():
     for event in pygame.event.get(QUIT):
-        exit()
+        terminate()
 
     for event in pygame.event.get(KEYDOWN):
         if event.key == K_ESCAPE:
-            exit()
+            terminate()
         if event.mod == KMOD_LCTRL and event.key == K_w:
-            exit()
+            terminate()
         pygame.event.post(event) # put the other KEYDOWN events back to the list
 
 def main():
     while True:
         checkQuit()
+
+        pygame.display.update()
 
 if __name__ == main():
     main()
